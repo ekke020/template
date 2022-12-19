e2750b74a07644efd09b7b8236f778fefa04fc78
Johan Ekman <johan.ekman@pricer.com>
2022-12-19 10:12:38 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo Compiling stuff
+gcc -o compiled test.c
+echo so fast wow
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Very important code\n");
+    return 0;
+}
